5b67240a0f6a10831cdefd5cae43c711fb02fea6
mingjingsi <30649118+mingjingsi@users.noreply.github.com>
2019-01-18 16:31:39 +0800

    Add files via upload

---
+++ realdata_example.R
@@ -0,0 +1,29 @@
+##### An example to reproduce the real data analysis #####
+# Schizophrenia 2 (Schizophrenia Psychiatric Genome-Wide Association Study (GWAS) Consortium, 2011)
+# For other GWASs, the code is similar only with some modifications explained below
+# The data sets analyzed in this paper, including nine genic category annotations, 127 Tissue-specific functional annotations, and the summary statistics of 30 GWAS can be download at https://drive.google.com/drive/folders/1YrhncaNpo6doHfnIYqXPlhPdE7YpqEjO
+
+library(LSMM)
+
+load("SCZ1.RData") # load the summary statistics (notes: change the file name for other GWASs)
+load("region9.RData") # load the genic category annotations
+load("tissue127.RData") # load the cell-type specific annotations
+
+SCZ1_Pvalue <- SCZ1[, c(1, 8)] # extract the SNP IDs and P-values (notes: the number of the columns may be different for other GWASs)
+
+region9tissue127 <- as.data.frame(cbind(region9, tissue127))
+region9tissue127$SNP <- rownames(region9)
+  
+SCZ1_ZA1 <- merge(SCZ1_Pvalue, region9tissue127, by.x = "snpid", by.y = "SNP") # keep the SNPs shared in GWAS and annotations (notes: the column name of the SNP ID may be different for other GWASs)
+
+Pvalue <- SCZ1_ZA1[, 2]
+Z <- SCZ1_ZA1[, 3:11]
+A <- SCZ1_ZA1[, 12:138]
+
+fit.LSMM <- LSMM(Pvalue, Z, A)
+
+assoc.SNP.LSMM <- assoc.SNP(fit.LSMM, FDRset = 0.1, fdrControl="global")
+table(assoc.SNP.LSMM$gamma)
+
+relev.Anno.LSMM <- relev.Anno(fit.LSMM, FDRset = 0.1, fdrControl="local")
+table(relev.Anno.LSMM)
